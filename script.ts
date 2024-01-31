ad8a9aa6ba2f1edd10799f983a7778c36cf36a10
Соляников Егор <FioMio@stud.vvsu.ru>
2024-01-31 17:50:31 +1000

    31.01.2024

---
+++ script.ts
@@ -0,0 +1,46 @@
+// const arr = [1, 2, 3, 4, 5];
+
+// const printArray = (arr) => {
+//     for (let i = 0; i < arr.lenght; i++) {
+//         console.log(arr[i]);
+//     }
+// };
+
+// printArray(arr);
+
+
+// const num = 5;
+
+// const text:string = "arrqy";
+
+// const arr = [1, 2, 3];
+
+// const vid = function message (showmessage) {
+//     console.log ("1,2,3")
+// }
+
+class home{}
+    class Point{x: number; y: number;}
+    let point : Point = new Point();
+
+    function pv (){
+        point.x = 1;
+        point.y = 2;
+    }
+    pv();
+    console.log(point);
+
+        class Point2{
+        height: number; 
+        large: number;
+        }
+            let point2 : Point2 = new Point2();
+    function vp (){
+        point2.height = 2;
+        point2.large = 3;
+    }
+    vp();
+    console.log(point2);
+    
+        
+        
